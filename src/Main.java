96e3b0cfe437b5a7f904c7740006689eec58d2a6
csa21472001 <21461901as@gmail.com>
2023-06-02 15:59:37 +0300

    Доп ветка. Финал2

--- src/Main.java
+++ src/Main.java
@@ -13,8 +13,8 @@ public class Main {
 //        task9();
 //        task10();
     }
-    public static void task1()
-    {
+
+    public static void task1() {
         System.out.println("Задача 1");
         int total = 0;
         int monthlyFee = 15000;
@@ -25,8 +25,8 @@ public class Main {
             System.out.println(" Месяц " + i + " сумма накопления равна " + total + " рублей");
         }
     }
-    public static void task2()
-    {
+
+    public static void task2() {
         System.out.println("Задача 2");
         int i = 0;
         while (i <= 10) {
@@ -39,8 +39,8 @@ public class Main {
             System.out.print(i + " ");
         }
     }
-    public static void task3()
-    {
+
+    public static void task3() {
         System.out.println("Задача 3");
         int totalPeople = 12000000;
         int birthRate = 17 * (12000000 / 1000);
@@ -52,20 +52,20 @@ public class Main {
             System.out.println(" Год " + i + " численность населения составляет " + totalPeople);
         }
     }
-    public static void task4()
-    {
+
+    public static void task4() {
         System.out.println("Задача 4");
         double totalDeposit = 15000.0;
         int i = 0;
-        while (totalDeposit < 12_000_000 ) {
-            totalDeposit = totalDeposit*1.07;
+        while (totalDeposit < 12_000_000) {
+            totalDeposit = totalDeposit * 1.07;
             i++;
-            System.out.printf(" Месяц %d сумма накоплений составляет %.3f ",i, totalDeposit);
+            System.out.printf(" Месяц %d сумма накоплений составляет %.3f ", i, totalDeposit);
             System.out.println();
         }
     }
-    public static void task5()
-    {
+
+    public static void task5() {
         System.out.println("Задача 5");
         double totalDeposit = 15000.0;
         int i = 0;
@@ -73,18 +73,18 @@ public class Main {
             totalDeposit = totalDeposit * 1.07;
             i++;
             if (i % 6 == 0) {
-                System.out.printf(" Месяц %d сумма накоплений составляет %.3f ",i, totalDeposit);
+                System.out.printf(" Месяц %d сумма накоплений составляет %.3f ", i, totalDeposit);
                 System.out.println();
             }
         }
     }
-    public static void task6()
-    {
+
+    public static void task6() {
         System.out.println("Задача 6");
         double totalDeposit = 15000.0;
-        int durationOfDeposit = 9*12;
+        int durationOfDeposit = 9 * 12;
         int i = 0;
-        while (i<=durationOfDeposit) {
+        while (i <= durationOfDeposit) {
             totalDeposit = totalDeposit + totalDeposit * 0.07;
             i++;
             if (i % 6 == 0) {
@@ -92,27 +92,27 @@ public class Main {
             }
         }
     }
-    public static void task7()
-    {
+
+    public static void task7() {
         System.out.println("Задача 7");
         int friday = 5;// первая пятница месяца
         //int i = 1;
-        while (friday<=31) {
-                System.out.println(" Сегодня пятница, " + friday + "-е число. Необходимо подготовить отчет");
-            friday+=7;
-        }
-    }
-    public static void task8()
-    {
-        System.out.println("Задача 8");
-        int  i = 0;
-        while (i<=2123) {
-            if (i<=2123 && i>=1823){
-            System.out.println(" Сегодня заканчивается " + i + " год. Комета пролетела");}
-            i+=79;
+        while (friday <= 31) {
+            System.out.println(" Сегодня пятница, " + friday + "-е число. Необходимо подготовить отчет");
+            friday += 7;
         }
     }
 
+    public static void task8() {
+        System.out.println("Задача 8");
+        int i = 0;
+        while (i <= 2123) {
+            if (i <= 2123 && i >= 1823) {
+                System.out.println(" Сегодня заканчивается " + i + " год. Комета пролетела");
+            }
+            i += 79;
+        }
+    }
 
 
 }
